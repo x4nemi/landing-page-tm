ba91caf6871602697052ac68916b79afc7f800ab
x4nemi <ximena.garcia7482@alumnos.udg.mx>
2022-09-08 00:48:42 -0500

    Formulario con código

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import Input from '../elements/Input';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import Input from "../elements/Input";
+import Button from "../elements/Button";
 
 const propTypes = {
   ...SectionProps.types,
-  split: PropTypes.bool
-}
+  split: PropTypes.bool,
+};
 
 const defaultProps = {
   ...SectionProps.defaults,
-  split: false
-}
+  split: false,
+};
+
+const names = [
+  "Joressa",
+  "Ximena",
+  "Luis",
+  "Juan",
+  "Pedro",
+  "Maria",
+  "Jose",
+  "Adela",
+]; //8
+const comments = [
+  "Hola",
+  "Como estas",
+  "Que tal",
+  "Buenas tardes",
+  "Buenas noches",
+  "Buenos dias",
+  "Buenas tardes",
+  "Buenas noches",
+]; // 8
+const emails = [
+  "@email.com",
+  "@hotmail.com",
+  "@gmail.com",
+  "@yahoo.com",
+  "@outlook.com",
+  "@live.com",
+  "@aol.com",
+  "@icloud.com",
+]; //8
+
+const intRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 const Cta = ({
   className,
@@ -25,49 +61,105 @@ const Cta = ({
   split,
   ...props
 }) => {
-
   const outerClasses = classNames(
-    'cta section center-content-mobile reveal-from-bottom',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
+    "cta section center-content-mobile reveal-from-bottom",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
     className
   );
 
   const innerClasses = classNames(
-    'cta-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider',
-    split && 'cta-split'
-  );  
+    "cta-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider",
+    split && "cta-split"
+  );
+
+  const [email, setEmail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [comment, setComment] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleCode = (value) => {
+    setCode(value);
+    const index = intRandom(0, 8);
+    if (value === "1234") {
+      setName(names[index]);
+      setEmail(names[index] + emails[intRandom(0, 8)]);
+      setComment(comments[intRandom(0, 8)]);
+    } else {
+      setName("");
+      setEmail("");
+      setComment("");
+    }
+  };
 
   return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
+    <section {...props} className={outerClasses}>
       <div className="container">
-        <div
-          className={innerClasses}
-        >
+        <div className={innerClasses}>
           <div className="cta-slogan">
             <h3 className="m-0">
-              For previewing layouts and visual?
-              </h3>
+              ¿Te interesa formar parte de nuestro equipo?
+            </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your best email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+            {/* <div className="m-10">Nombre</div> */}
+            <Input
+              id="text"
+              type="text"
+              label="text"
+              labelHidden
+              placeholder="Nombre"
+              className="mt-16"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Input>
+
+            <Input
+              id="newsletter"
+              type="email"
+              label="Subscribe"
+              labelHidden
+              placeholder="Correo electrónico"
+              className="mt-16"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Input>
+
+            <Input
+              id="textarea"
+              type="textarea"
+              label="textarea"
+              labelHidden
+              placeholder="Comentario"
+              className="mt-16"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            ></Input>
+
+            <Input
+              id="text"
+              type="text"
+              label="text"
+              labelHidden
+              placeholder="Código"
+              className="mt-16"
+              value={code}
+              onChange={(e) => handleCode(e.target.value)}
+            ></Input>
+
+            <Button tag="a" color="white" className="mt-16" wideMobile href="#">
+              Enviar
+            </Button>
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
 Cta.propTypes = propTypes;
 Cta.defaultProps = defaultProps;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 // import sections
-import Hero from '../components/sections/Hero';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
+import Hero from "../components/sections/Hero";
+import FeaturesTiles from "../components/sections/FeaturesTiles";
+import FeaturesSplit from "../components/sections/FeaturesSplit";
+import Testimonial from "../components/sections/Testimonial";
 
 const Home = () => {
-
   return (
     <>
       <Hero className="illustration-section-01" />
       <FeaturesTiles />
-      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
+      <FeaturesSplit
+        invertMobile
+        topDivider
+        imageFill
+        className="illustration-section-02"
+      />
       <Testimonial topDivider />
-      <Cta split />
     </>
   );
-}
+};
 
 export default Home;
